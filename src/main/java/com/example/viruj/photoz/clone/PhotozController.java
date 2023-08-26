abd21b42d5d22097e7ff96562a2ee927bb9480a4
viruj <virujthakur01@gmail.com>
2023-08-26 12:36:07 +0530

    initial commit

---
+++ src/main/java/com/example/viruj/photoz/clone/PhotozController.java
@@ -0,0 +1,13 @@
+package com.example.viruj.photoz.clone;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PhotozController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World!";
+    }
+}
